af83cbe2c2a2583fd0c2738faf3a0ced42062444
Vishal Babu <vishalbabu422@gmail.com>
2025-02-25 16:43:54 +0530

    difference between fn declaration and expression

--- funExprAndDecl.js
+++ funExprAndDecl.js
@@ -1,10 +1,20 @@
-/* function expression */
+/* function expression,cannot be hoisted,name is not necessary, can be IIFE */
 let hello = function () {
   console.log("Hello");
 };
 
-/* function declaration */
+let hello2 = function greet() {
+  console.log("Hello");
+};
 
-function hello1(){
-    console.log("Hello");
+///IIFE ///
+
+(function () {
+  console.log("Hello");
+})();
+
+/* function declaration, since it is a declaration it is hoisted, name is necessary */
+
+function hello1() {
+  console.log("Hello");
 }
