0fe54c538dbbb764453fcb490518c77060e58896
Vishal Babu <vishalbabu422@gmail.com>
2025-02-25 17:12:17 +0530

    closure with example

--- closure.js
+++ closure.js
@@ -1,3 +1,4 @@
+/* closure is a function that remembers its lexical scoping even after the function that has created it has stopped executing . retains access of variables from its outside function. Advantages : Encapsulation and state mgmt. Uses: callbacks,setTimeOut */
 function outer() {
   let counter = 0;
 
